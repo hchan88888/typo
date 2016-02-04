050bf4ef3739b441165d5b1d9e2ad2d82c7ff581
hchan88888@gmail.com <hchan88888@gmail.com>
2016-02-04 05:30:08 +0000

    second fix

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -151,11 +151,11 @@ class Admin::ContentController < Admin::BaseController
     
     
     #added to avoid loop for profile_id =3 login
-    #unless current_user.profile_id != 3
-    #  redirect_to :action => 'index'
-    #  flash[:error] = _("Error, you are not allowed to perform this action")
-    #  return
-    #end
+    unless current_user.profile_id != 3
+      redirect_to :action => 'index'
+      flash[:error] = _("Error, you are not allowed to perform this action")
+      return
+    end
     ### addition ended
     id = params[:id]
     id = params[:article][:id] if params[:article] && params[:article][:id]
